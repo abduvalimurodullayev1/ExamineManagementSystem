1c25ae6f8942534a806d5298ee4c670a6b0c09cb
abduvalimurodullayev <abduvalimurodullayev9@gmail.com>
2025-03-25 10:24:51 +0500

    Add apps

---
+++ apps/users/signals.py
@@ -0,0 +1,19 @@
+# apps/users/signals.py
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from common.models import Assignment, Submission, Notification
+
+
+@receiver(post_save, sender=Assignment)
+def create_assignment_notification(sender, instance, created, **kwargs):
+    if created:
+        message = f"New assignment assigned: {instance.exam.exam_name}"
+        Notification.objects.create(user=instance.student, message=message)
+
+
+@receiver(post_save, sender=Submission)
+def create_submission_notification(sender, instance, created, **kwargs):
+    if created:
+        message = f"Your submission for {instance.exam.exam_name} has been received."
+        Notification.objects.create(user=instance.student, message=message)
